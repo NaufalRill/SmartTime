1e0797ffdc8cad9e9d384484effaf00949d794ae
NaufalGR <naufalghifari1139@gmail.com>
2026-01-06 00:10:09 +0700

    Add push notification

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,8 @@
 import { Redirect } from "expo-router";
 
+import registerNNPushToken from 'native-notify';
+
 export default function Index() {
+  registerNNPushToken(33100, 'X5ZtQrSbAfozDexPPqKkwX');
   return <Redirect href="/auth/login" />;
 }
