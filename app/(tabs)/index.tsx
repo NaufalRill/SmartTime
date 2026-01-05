3cc4ac1e2cc4f94be0fb89e14f3eb792dc7a3f61
NaufalGR <naufalghifari1139@gmail.com>
2026-01-06 01:38:18 +0700

    Remove native notify

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,8 +1,6 @@
 import { Redirect } from "expo-router";
 
-import registerNNPushToken from 'native-notify';
 
 export default function Index() {
-  registerNNPushToken(33100, 'X5ZtQrSbAfozDexPPqKkwX');
   return <Redirect href="/auth/login" />;
 }
